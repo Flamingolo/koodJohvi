778fa11901a8359f28230d3ada03184956793a04
Ingo <ingo.hark@gmail.com>
2024-04-07 10:17:35 +0300

    register

---
+++ backend/handlers/register.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"main/models"
+	"main/utils"
+	"net/http"
+)
+
+func RegisterHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var user models.User
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+
+		user.PasswordHash, err = utils.HashPassword(user.Password)
+		if err != nil {
+			http.Error(w, "Error while hashing the password", http.StatusInternalServerError)
+			return
+		}
+
+		err = models.CreateUser(db, user)
+		if err != nil {
+			http.Error(w, "Error while creating the user", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("Account successfully created"))
+	}
+
+}
--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"log"
+	"main/handlers"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
+	db, err := sql.Open("sqlite3", "forum.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", handleWebSocket)
-	http.ListenAndServe(":8080", nil)
+	// Handlers
+	http.HandleFunc("/backend/register", handlers.RegisterHandler(db))
+
+	// Server
+	log.Println("Server starting on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
 
 }
